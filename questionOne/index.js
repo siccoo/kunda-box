00ef8bfc861b1ecb8557d0d245ebed4c6a99a9de
Michael Chilaka <obichilaka@gmail.com>
2023-03-20 12:35:37 +0100

    fixing database bugs

--- questionOne/index.js
+++ questionOne/index.js
@@ -12,10 +12,10 @@ var pool = new Pool({
     password: '12345678',
     port: 13958,
     ssl: true,
-    max: 20, // set pool max size to 20
+    // max: 20, // set pool max size to 20
     // idleTimeoutMillis: 1000, // close idle clients after 1 second
     // connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-    maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+    // maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
 })
 
 // Define the insert_user function
