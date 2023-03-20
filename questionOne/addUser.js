b0c67d0c357d9c73ca07536ea6e3a7440b3baebe
Michael Chilaka <obichilaka@gmail.com>
2023-03-20 14:07:18 +0100

    updates on addUser

--- questionOne/addUser.js
+++ questionOne/addUser.js
@@ -2,7 +2,7 @@ const insert_user  = require("./index")
 
 async function addUser() {
     const user = {
-        user_name: 'joe',
+        user_name: 'joe_kunda',
         dob: new Date(1990, 1, 1),
         email: 'jo@kundabox.com',
         password: '12ABCabc '
